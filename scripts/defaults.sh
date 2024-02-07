ece90a5f54e59650ca3ec009e7051d69d690e2ab
Colby <colby.braden@gmail.com>
2024-02-06 22:10:11 -0600

    updated scripts and configs

---
+++ scripts/defaults.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+## Default programs
+sudo apt install xserver-xorg xorg lxpolkit rsync gtk2-engines-murrine \
+    fonts-ubuntu fonts-dejavu fonts-font-awesome fonts-liberation2 \
+    fonts-noto fonts-roboto libxkbcommon-dev lxappearance \
+    pavucontrol materia-gtk-theme arc-theme gnome-themes-extra \
+    papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev \
+    galculator geany dunst libnotify-bin suckless-tools build-essential curl \
+    wget sxiv scrot feh neofetch htop mpv arc-theme synaptic gamemode wine \
+    bspwm sxhkd i3status python3-i3ipc playerctl cups pipewire-audio -y
+
---
+++ scripts/flatpak.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install flatpak -y &&
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
