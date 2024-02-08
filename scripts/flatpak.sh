df4d7242fce39afa7803cd56692e62d7d7f10abe
Colby <colby.braden@gmail.com>
2024-02-07 18:07:05 -0600

    updated config

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt install flatpak -y &&
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
