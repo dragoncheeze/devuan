2b4433505f0ca8d8e4e304096ce2c2d29c4cf18d
Colby <colby.braden@gmail.com>
2024-10-08 07:55:49 -0500

    updated default install script

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -2,10 +2,10 @@
 ## Default programs
 sudo apt install xserver-xorg xorg lxpolkit rsync gtk2-engines-murrine \
     fonts-ubuntu fonts-dejavu fonts-font-awesome fonts-liberation2 \
-    fonts-noto fonts-roboto libxkbcommon-dev lxappearance \
-    pavucontrol materia-gtk-theme arc-theme gnome-themes-extra \
+    fonts-noto fonts-roboto libxkbcommon-dev lxappearance picom \
+    pavucontrol materia-gtk-theme arc-theme gnome-themes-extra distrobox podman \
     papirus-icon-theme pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev \
     galculator geany dunst libnotify-bin suckless-tools build-essential curl \
     wget sxiv scrot feh neofetch htop mpv arc-theme synaptic gamemode wine \
-    i3status python3-i3ipc playerctl cups alsa-utils pulseaudio pulsemixer -y
+    i3status python3-i3ipc playerctl cups alsa-utils pipewire-audio pulsemixer -y
 
