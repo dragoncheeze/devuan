df9c873692a9747300df6bf5d8fe30504042ce3e
Colby <colby.braden@gmail.com>
2025-03-09 09:42:33 -0500

    updated sway installs script

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo dpkg --add-architecture i386 && sudo apt update &&
 sudo apt install sway swaybg swaylock swayidle \
-    python3-i3ipc seatd dunst light playerctl \
+    python3-i3ipc seatd dunst light playerctl autotiling \
     i3status alsa-utils pulseaudio pulsemixer pavucontrol libpam-ck-connector \
     wl-clipboard neofetch gtk2-engines-murrine mpv mpd foot \
     papirus-icon-theme arc-theme rofi fonts-dejavu fonts-font-awesome \
